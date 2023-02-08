6b39922514b980568593ef001fed4317209666fe
Lahari Sai K S <sailahari444@gmail.com>
2023-02-08 15:32:01 +0530

    "simple"

--- src/main/java/com/cakeshop/repository/OrdersRepo.java
+++ src/main/java/com/cakeshop/repository/OrdersRepo.java
@@ -1,11 +1,7 @@
 package com.cakeshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.cakeshop.entity.Orders;
-
-@Repository
 public interface OrdersRepo extends JpaRepository<Orders, Integer> {
 
 }
